8373c90bff6f059a528b1d6b244b2ec5e418ff48
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-07-16 08:59:18 -0400

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+// utils.js
+export const GRID_SIZE = 9;
+export const TOTAL_GRID_SIZE = 15;
+export const PLAYER_START_POSITION = { x: 7, y: 7 };
+export const INITIAL_HEALTH = 3;
+export const ATTACK_RANGE = 4;
